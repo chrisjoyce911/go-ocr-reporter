5dced18b512a8a61dd9f7cead92ed1bda1726ca9
Chris Joyce <chris@integratedcapital.io>
2020-05-26 15:29:54 +1000

    Added date range to report

---
+++ octopusreporter_test.go
@@ -0,0 +1,48 @@
+package ocr
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOCR_LoadCustomers(t *testing.T) {
+
+	tests := []struct {
+		name    string
+		file    string
+		want    []Customer
+		wantErr bool
+	}{
+		{
+			name:    "ok",
+			file:    "LoadCustomers_ok.json",
+			wantErr: false,
+			want: []Customer{
+				{
+					ID:          2,
+					WsID:        "f8e508b7bf16",
+					InternalID:  "52",
+					Label:       "Customer_X",
+					OwnUse:      0,
+					AfterImport: "user@ou=%/Customer_X/%\r\nserver@ou=%/Customer_X/%",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			o := &OCR{}
+			file := fmt.Sprintf("test_files/%s", tt.file)
+			data, _ := ioutil.ReadFile(file)
+			got, err := o.LoadCustomers(data)
+
+			if assert.NoError(t, err) {
+				assert.Equal(t, got, tt.want)
+			}
+
+		})
+	}
+}
---
+++ unmarshal.go
@@ -0,0 +1,83 @@
+package ocr
+
+import (
+	"strconv"
+	"strings"
+	"time"
+)
+
+type SpecialBool struct {
+	bool
+}
+
+type SpecialDate struct {
+	time.Time
+}
+
+type SpecialUnix struct {
+	time.Time
+}
+
+type SpecialInt struct {
+	int
+}
+
+func (sd *SpecialDate) UnmarshalJSON(input []byte) error {
+	strInput := string(input)
+	strInput = strings.Trim(strInput, `"`)
+
+	var newTime time.Time
+	var err error
+	if strInput != "null" {
+		newTime, err = time.Parse("2006-01-02 15:04:05", strInput)
+		if err != nil {
+			return err
+		}
+	}
+	sd.Time = newTime
+	return nil
+}
+
+func (sb *SpecialBool) UnmarshalJSON(input []byte) error {
+	strInput := string(input)
+	strInput = strings.Trim(strInput, `"`)
+	switch strInput {
+	case "0":
+		sb.bool = false
+	case "1":
+		sb.bool = true
+	case "No":
+		sb.bool = false
+	case "Yes":
+		sb.bool = true
+	}
+	return nil
+}
+
+func (st *SpecialUnix) UnmarshalJSON(input []byte) error {
+	strInput := string(input)
+	strInput = strings.Trim(strInput, `"`)
+
+	i, err := strconv.ParseInt(strInput, 10, 64)
+	if err != nil {
+		return err
+	}
+	st.Time = time.Unix(i, 0)
+	return nil
+}
+
+func (si *SpecialInt) UnmarshalJSON(input []byte) error {
+	strInput := string(input)
+	strInput = strings.Trim(strInput, `"`)
+
+	var i int
+	var err error
+
+	i, err = strconv.Atoi(strInput)
+	if err != nil {
+		return err
+	}
+
+	si.int = i
+	return nil
+}
